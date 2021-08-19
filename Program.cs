494ffdaf713a5671801e8f14f24a48cf4ec5b214
George S <george@synteda.se>
2021-08-19 10:58:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Programmering_Förberedande_kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Menu
+            Console.WriteLine("Meny");
+            Console.WriteLine("-------------");
+            Console.WriteLine("1) Funktion som skriver ut \'Hello World\' i konsolen");
+            Console.WriteLine("2) Funktion som tar in input från användaren (Förnamn, Efternamn, Ålder) och sedan skriver ut dessa i konsolen");
+
+            //Menu input
+            Console.WriteLine("\nType option and press [Enter]");
+            int menuOption = Convert.ToInt32(Console.ReadLine());
+
+            //Switch for menu option
+            switch (menuOption)
+            {
+                case 1:
+                    Console.WriteLine("Hello World!");
+                    break;
+
+                case 2:
+                    string firstName;
+                    string lastName;
+                    string age;
+
+                    Console.WriteLine("Förnamn: ");
+                    firstName = Console.ReadLine();
+                    Console.WriteLine("Efternamn: ");
+                    lastName = Console.ReadLine();
+                    Console.WriteLine("Ålder: ");
+                    age = Console.ReadLine();
+
+                    Console.WriteLine("\nNamn: "+firstName + " " + lastName + "\nÅlder: " + age);
+                    break;
+
+                default:
+                    Console.WriteLine("Alternativet finns inte i urvalet.");
+                    break;
+            }
+        }
+    }
+}
